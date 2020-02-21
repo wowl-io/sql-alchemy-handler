dc359b42677c6c1cd2d0442bfca17e62e261d63c
Spencer Siu <spencer@topocean.com>
2020-02-21 12:59:06 -0800

    Fixed url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="SQL Alchemy helpers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/topocean/python-app-includes",
+    url="https://github.com/topocean/sql-alchemy-handler",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
